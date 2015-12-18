31bead167fb11bd930e298542a99b9a779e99d71
joinAero <join.aero@gmail.com>
2015-12-18 17:41:10 +0800

    init repo

---
+++ app/src/main/java/org/join/turbo/MyApplication.java
@@ -0,0 +1,12 @@
+package org.join.turbo;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
