977fda557bcfd00b346ae9aff088cc1a2340da5b
joinAero <join.aero@gmail.com>
2015-12-29 16:10:11 +0800

    add user visible to base fragment

--- app/src/main/java/cc/cubone/turbo/ui/base/BaseFragment.java
+++ app/src/main/java/cc/cubone/turbo/ui/base/BaseFragment.java
@@ -4,7 +4,36 @@ import android.support.v4.app.Fragment;
 
 public class BaseFragment extends Fragment {
 
+    private boolean mUserNotSeen = true;
+
     public BaseFragment() {
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (mUserNotSeen && isVisibleToUser) {
+            onUserFirstSight();
+            mUserNotSeen = false;
+        }
+        onUserVisibleChanged(isVisibleToUser);
+    }
+
+    /**
+     * Called when visible to user first time.
+     */
+    protected void onUserFirstSight() {
+    }
+
+    /**
+     * Called when visible to user has changed.
+     *
+     * <ul>
+     * <li><a href="http://stackoverflow.com/questions/10024739/how-to-determine-when-fragment-becomes-visible-in-viewpager">
+     * How to determine when Fragment becomes visible in ViewPager</a>
+     * </ul>
+     */
+    protected void onUserVisibleChanged(boolean visible) {
     }
 
 }
