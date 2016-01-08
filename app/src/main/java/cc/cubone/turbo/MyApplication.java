64909e6adda2ca5ca0cd4e0c12aecb092f602685
joinAero <join.aero@gmail.com>
2016-01-08 13:17:17 +0800

    add strict mode

--- app/src/main/java/cc/cubone/turbo/MyApplication.java
+++ app/src/main/java/cc/cubone/turbo/MyApplication.java
@@ -1,6 +1,7 @@
 package cc.cubone.turbo;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import cc.cubone.turbo.core.debug.CrashHandler;
 import cc.cubone.turbo.core.util.Log;
@@ -13,7 +14,25 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Log.setLog(DEBUG);
+        if (DEBUG) {
+            setupStrictMode();
+        }
         new CrashHandler(this);
+    }
+
+    private void setupStrictMode() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()
+                .penaltyLog()
+                .build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build());
     }
 
 }
