68fb77ee8be05a8ab409a66a1600102dfab67c27
joinAero <join.aero@gmail.com>
2016-01-14 23:59:23 +0800

    add annotation for static int

--- app/src/main/java/cc/cubone/turbo/persistence/PrefAllApps.java
+++ app/src/main/java/cc/cubone/turbo/persistence/PrefAllApps.java
@@ -1,8 +1,22 @@
 package cc.cubone.turbo.persistence;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 public class PrefAllApps extends Pref {
+
+    @IntDef({LAYOUT_LIST, LAYOUT_GRID})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Layout {}
+
+    @IntDef(flag = true, value = {FLAG_DISPLAY_SYSTEM, FLAG_DISPLAY_USER,
+            FLAG_DISPLAY_STOPPED, FLAG_DISPLAY_RUNNING})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface FlagDisplay {}
 
     public static final int LAYOUT_LIST = 0;
     public static final int LAYOUT_GRID = 1;
@@ -10,6 +24,7 @@ public class PrefAllApps extends Pref {
     public static final int FLAG_DISPLAY_SYSTEM = 0x00000001;
     public static final int FLAG_DISPLAY_USER = FLAG_DISPLAY_SYSTEM<<1;
 
+    @SuppressLint("ShiftFlags")
     public static final int FLAG_DISPLAY_STOPPED = 0x00000100;
     public static final int FLAG_DISPLAY_RUNNING = FLAG_DISPLAY_STOPPED<<1;
 
@@ -20,21 +35,25 @@ public class PrefAllApps extends Pref {
         super(context, "all_apps");
     }
 
+    @SuppressWarnings("ResourceType")
+    @Layout
     public int getLayout() {
         return mSp.getInt(KEY_LAYOUT, LAYOUT_LIST);
     }
 
-    public void setLayout(int layout) {
+    public void setLayout(@Layout int layout) {
         mSp.edit().putInt(KEY_LAYOUT, layout).apply();
     }
 
+    @SuppressWarnings("ResourceType")
+    @FlagDisplay
     public int getDisplayFlags() {
         return mSp.getInt(KEY_DISPLAY_FLAGS, FLAG_DISPLAY_USER
                 | FLAG_DISPLAY_STOPPED
                 | FLAG_DISPLAY_RUNNING);
     }
 
-    public void setDisplayFlags(int displayFlags) {
+    public void setDisplayFlags(@FlagDisplay int displayFlags) {
         mSp.edit().putInt(KEY_DISPLAY_FLAGS, displayFlags).apply();
     }
 }
