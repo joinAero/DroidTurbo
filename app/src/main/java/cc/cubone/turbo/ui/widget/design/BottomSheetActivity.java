5cb1ee39b0c6e6d62afce82d64707cdf14ef88c1
joinAero <join.aero@gmail.com>
2016-09-22 20:33:05 +0800

    use bottom sheet callback

--- app/src/main/java/cc/cubone/turbo/ui/widget/design/BottomSheetActivity.java
+++ app/src/main/java/cc/cubone/turbo/ui/widget/design/BottomSheetActivity.java
@@ -1,10 +1,12 @@
 package cc.cubone.turbo.ui.widget.design;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -24,6 +26,7 @@ public class BottomSheetActivity extends BaseActivity {
     Toolbar mBottomSheetToolbar;
 
     private BottomSheetBehavior mBehavior;
+    private int mBottomSheetBarLayoutHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +39,32 @@ public class BottomSheetActivity extends BaseActivity {
     private void initViews() {
         initToolbar();
         mBehavior = BottomSheetBehavior.from(mBottomSheet);
+        mBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+            }
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                setViewHeight(mBottomSheetBarLayout,
+                        (int) (mBottomSheetBarLayoutHeight * slideOffset));
+            }
+        });
         mBottomSheetBarLayout = ButterKnife.findById(mBottomSheet, R.id.layout_bar);
         mBottomSheetToolbar = ButterKnife.findById(mBottomSheetBarLayout, R.id.tool_bar);
+        mBottomSheetToolbar.setTitle(R.string.bottom_sheet);
+        mBottomSheetToolbar.setNavigationIcon(android.support.v7.appcompat.R.drawable.abc_ic_clear_material);
+        mBottomSheetToolbar.setNavigationOnClickListener(v -> mBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED));
         holdStatusBar(mBottomSheetBarLayout);
+        mBottomSheetBarLayout.measure(
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        mBottomSheetBarLayoutHeight = mBottomSheetBarLayout.getMeasuredHeight();
+        setViewHeight(mBottomSheetBarLayout, 0);
     }
 
+    private void setViewHeight(View v, int h) {
+        ViewGroup.LayoutParams params = v.getLayoutParams();
+        params.height = h;
+        v.setLayoutParams(params);
+    }
 }
