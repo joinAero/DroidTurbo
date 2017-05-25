f9c40db6a3b47c5ee9dfc64d1b57bf3312c41fe4
joinAero <join.aero@gmail.com>
2017-05-25 11:02:39 +0800

    Update cpp base and readme

--- app/src/main/cpp/base/global.hpp
+++ app/src/main/cpp/base/global.hpp
@@ -1,23 +1,38 @@
-/**
- * <p>Macros for different platforms:</p>
- * <ul>
- * <li>OS_ANDROID: Android
- * <li>OS_IPHONE: iOS
- * </ul>
- */
 #ifndef GLOBAL_HPP_
 #define GLOBAL_HPP_
 #pragma once
 
 // http://stackoverflow.com/questions/5919996/
 // how-to-detect-reliably-mac-os-x-ios-linux-windows-in-c-preprocessor
-#if defined(__APPLE__)
-#  include "TargetConditionals.h"
-#  if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
-#    define OS_IPHONE
-#  endif
-#elif defined(__ANDROID__)
-#  define OS_ANDROID
+#ifdef _WIN32
+    #define OS_WIN
+    #ifdef _WIN64
+        #define OS_WIN64
+    #else
+        #define OS_WIN32
+    #endif
+#elif __APPLE__
+    #include "TargetConditionals.h"
+    #if TARGET_IPHONE_SIMULATOR
+        #define OS_IPHONE
+        #define OS_IPHONE_SIMULATOR
+    #elif TARGET_OS_IPHONE
+        #define OS_IPHONE
+    #elif TARGET_OS_MAC
+        #define OS_MAC
+    #else
+        #error "Unknown Apple platform"
+    #endif
+#elif __ANDROID__
+    #define OS_ANDROID
+#elif __linux__
+    #define OS_LINUX
+#elif __unix__
+    #define OS_UNIX
+#elif defined(_POSIX_VERSION)
+    #define OS_POSIX
+#else
+    #error "Unknown compiler"
 #endif
 
 #define DISABLE_COPY(Class) \
