040f240d3c44ffed1f40bcdfce5f82ace31b768c
joinAero <join.aero@gmail.com>
2017-10-20 22:51:06 +0800

    Change all ndkbuild to cmake

--- libopencv/src/main/cpp/Android.mk
+++
@@ -1,8 +0,0 @@
-MY_LOCAL_PATH := $(call my-dir)
-MY_MODULE_PATH := $(MY_LOCAL_PATH)/../../..
-MY_PROJECT_PATH := $(MY_MODULE_PATH)/..
-
-OPENCV_SDK_PATH := $(MY_PROJECT_PATH)/OpenCV-android-sdk
-#$(info OPENCV_SDK_PATH=$(OPENCV_SDK_PATH))
-
-include $(call all-subdir-makefiles)
